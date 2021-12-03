27660bc097f39fd799714da9c208c70bb3660825
saraatla <saraatlala@gmail.com>
2021-12-03 16:52:59 +0000

    edit

--- ui/employee_ui.py
+++ ui/employee_ui.py
@@ -20,7 +20,7 @@ class EmployeeUI:
             if commands2 == "1":
                 self.llapi.edit_employee(self.employee)
                 print(LINE)
-                edited_employee = self.llapi.search_employee(self.employee.ssn)
+                edited_employee = self.llapi.search_employee() # BREYTA ÞESSU
                 print(edited_employee)
                 print(LINE)
             elif commands2 == "B":
