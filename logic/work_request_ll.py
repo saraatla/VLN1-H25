2d8a3cd4b1ee6c0b3a235b6b8e6f0e35f89b2318
kristinbodvars <kristinb20@ru.is>
2021-12-03 14:01:43 +0000

    LL breytt

--- logic/work_request_ll.py
+++ logic/work_request_ll.py
@@ -1,6 +1,7 @@
 from data.DLAPI import DLAPI
 
 class WorkRequestLL:
+    """Work Request logic layer class; Contains X functions: fetches the functions in the data layer API,"""
     def __init__(self):
         self.dlapi = DLAPI()
 
