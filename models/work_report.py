7d5d7e938c527e8725737193552ab260d1af6cfa
Viktoria Ros Bjarnthorsdottir <viktoriaros@Viktorias-MacBook-Air-2.local>
2021-12-03 11:52:54 +0000

    Fixed work report model

--- models/work_report.py
+++ models/work_report.py
@@ -1,6 +1,6 @@
 class WorkReport:
-        def __init__(self,ssn,contractor, contractor_review, contractor_remuneration, total_cost,description, approved = False):
-                self.report_id
+        def __init__(self,workreport_id,ssn,contractor,contractor_review,contractor_remuneration,total_cost,description,approved = False):
+                self.workreport_id = workreport_id
                 self.ssn = ssn
                 self.contractor = contractor 
                 self.contractor_review = contractor_review
@@ -10,7 +10,14 @@ class WorkReport:
                 self.approved = approved
 
         def __str__(self):
-                return f"Workrequest_ID: {self.workrequest_id}, Title: {self.title}, Property_ID: {self.property_id}, Destination_ID: {self.destination_id}, Contractor: {self.contractor}, Repeat: {self.repeat}, When: {self.when}, Status: {self.status}, Priority: {self.priority}, Description: {self.description}"
+                return f"""Workreport_ID: {self.workreport_id} 
+SSN: {self.ssn}
+Contractor: {self.contractor}
+Contractor_review: {self.contractor_review}
+Contractor_remuneration: {self.contractor_remuneration}
+Total_cost: {self.total_cost}
+Desctiption: {self.description}
+Approved: {self.approved}"""
 
 
             
