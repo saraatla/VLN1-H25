6fcb7bd49a67bb3a78764d3f19cbb0ec1ef8d338
saraatla <saraatlala@gmail.com>
2021-12-03 15:20:10 +0000

    llapi lagað meira

--- ui/work_requests_menu_ui.py
+++ ui/work_requests_menu_ui.py
@@ -17,15 +17,15 @@ class WorkRequestMenu:
                 return
             operation = operations[selected_operation]
             if operation  == 'Search by SSN': # HALDA ÁFRAM HÉR SARA
-                found_employee = llapi.search_employees(self.location)
+                found_employee = self.llapi.search_employees(self.location)
                 if found_employee is not None:
                     employee_ui = EmployeeUI(found_employee)
                     employee_ui.start()
             elif operation == 'See list':
-                employee_list = llapi.list_employees(self.location)
+                employee_list = self.llapi.list_employees(self.location)
                 for employee in employee_list: #eh svona veit ekki
                     print(employee)
             elif operation == 'Add new':
-                new_employee = llapi.create_employee()
+                new_employee = self.llapi.create_employee()
                 employee_ui = EmployeeUI(new_employee)
                 employee_ui.start()
