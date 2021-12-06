6f87aece130200123f6510a1d6ec756b8db336b7
Daniel Amos Danielsson <89992816+daniel-amos@users.noreply.github.com>
2021-12-06 13:56:36 +0000

    update

--- ui/work_requests_menu_ui.py
+++ ui/work_requests_menu_ui.py
@@ -11,7 +11,7 @@ class WorkRequestMenu:
 
     def start(self):
          while True:
-            operations = ['Search by Work Request ID', 'Search by Property ID', 'Search by SSN', 'Search by Contractor Name' ,'See list of all requests', 'See list of requests ready to close' ,'Add new']
+            operations = ['Search by Work Request ID', 'Search by Property ID', 'Search by SSN', 'Search by Contractor Name', 'See list of all requests', 'See list of requests ready to close', 'Add new']
             operations_menu = Menu(f'Work Requests in {self.destination}\nChoose options', operations)
             selected_operation = operations_menu.draw_options()
             if selected_operation < 0:
@@ -22,26 +22,35 @@ class WorkRequestMenu:
                 if found_request is not None:
                     work_request_ui = WorkRequestUI(found_request, self.destination)
                     work_request_ui.start()
+
             elif operation == 'Search by Property ID':
                 found_request = self.llapi.search_work_requests()
                 if found_request is not None:
                     work_request_ui = WorkRequestUI(found_request, self.destination)
                     work_request_ui.start()
+
             elif operation == 'Search by SSN':
                 found_report = self.llapi.search_work_report()
                 if found_report is not None:
                     # pass
                     work_report_ui = WorkReportUI(found_report, self.destination)
                     work_report_ui.start()
+
             elif operation == 'Search by Contractor Name':
                 found_report = self.llapi.search_work_report()
                 if found_report is not None:
                     # pass
                     work_report_ui = WorkReportUI(found_report, self.destination)
                     work_report_ui.start()
+
             elif operation == 'See list of all requests':
-                employee_list = self.llapi.list_work_requests()
-                for employee in employee_list: #eh svona veit ekki
-                    print(employee)
+                workreq_list = self.llapi.list_work_requests()
+                for request in workreq_list:
+                    print(request)
+
+            elif operation == "See list of requests ready to close":
+                workreq_list = self.llapi.get_all_work_requests_by_status()
+                print(workreq_list)
+
             elif operation == 'Add new':
                 self.llapi.create_work_request()
