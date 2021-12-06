5a59ea96d9a3ece5dfbf0f2c8ef54190bb3183d3
saraatla <saraatlala@gmail.com>
2021-12-06 10:56:41 +0000

    property edit

--- data/DLAPI.py
+++ data/DLAPI.py
@@ -41,8 +41,8 @@ class DLAPI:
     def list_properties(self):
         return self.propertyDL.list_properties()
 
-    def edit_property(self,prop,col,newval):
-        return self.propertyDL.edit_property(prop,col,newval)
+    def edit_property(self,id,col,newval):
+        return self.propertyDL.edit_property(id,col,newval)
 
     # Contractor DL
     def create_contractor(self, cont):
