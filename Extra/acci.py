3cff7461dbb8f9809f1f1fa9dffcf2059f15a30d
Daniel Amos Danielsson <89992816+daniel-amos@users.noreply.github.com>
2021-12-06 09:02:02 +0000

    acci

--- Extra/acci.py
+++ Extra/acci.py
@@ -1,15 +1,15 @@
 import time
 from Extra.TermcolorFile.termcolor import colored
 def acci():
-        print(f'\x1bc{colored("███╗   ██╗ █████╗ ███╗   ██╗     █████╗ ██╗██████╗ ","red")}')
+        print(f'\x1b{colored("███╗   ██╗ █████╗ ███╗   ██╗     █████╗ ██╗██████╗ ","red")}')
         time.sleep(0.1)
         print(colored("████╗  ██║██╔══██╗████╗  ██║    ██╔══██╗██║██╔══██╗","green"))
-        time.sleep(0.1)
+        time.sleep(0.12)
         print(colored("██╔██╗ ██║███████║██╔██╗ ██║    ███████║██║██████╔╝", "blue"))
-        time.sleep(0.1)
+        time.sleep(0.13)
         print(colored("██║╚██╗██║██╔══██║██║╚██╗██║    ██╔══██║██║██╔══██╗", "yellow"))
-        time.sleep(0.1)
+        time.sleep(0.14)
         print(colored("██║ ╚████║██║  ██║██║ ╚████║    ██║  ██║██║██║  ██║", "magenta"))
-        time.sleep(0.1)
+        time.sleep(0.15)
         print(colored("╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═══╝    ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝", "cyan"))
-        time.sleep(0.1)
+        time.sleep(0.16)
