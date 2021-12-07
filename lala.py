3c3618984d9d620039e0bf3a2cf3991ebfe53c8e
kristinbodvars <kristinb20@ru.is>
2021-12-07 11:26:15 +0000

    tók út tilgangslaust

--- lala.py
+++
@@ -1,9 +0,0 @@
-
-
-
-for line in .splitlines():
-    if "wrep2" in line:
-        print(line.replace("wrep2", "wrep" + str(bla)))
-    elif "wrep3" in line:
-        print(line.replace("wrep3", "wrep" + str(bla)))
-    bla += 1
