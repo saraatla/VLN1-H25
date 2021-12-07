c3dff037223508a9645e48c3f02299fced45a374
kristinbodvars <kristinb20@ru.is>
2021-12-07 15:43:04 +0000

    cont og prob dl

--- data/property_dl.py
+++ data/property_dl.py
@@ -29,15 +29,15 @@ class PropertyDL:
             writer.writerow({'Destination': prop.destination, "Address": prop.address, "Squarefoot": prop.squarefoot, 
             "Rooms": prop.rooms, "Type": prop.type, "Property_ID": prop.property_id, "Facilities": prop.facilities})
 
-    def edit_property(self, id, col, newvalue):  
+    def edit_property(self, prop_id, col, new_value):  
         """This function edits a certain value for a certain property (input by Manager)"""
         with open(self.filepath, 'r', newline='', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile) # iterates over lines in the csvfile.
             data_list = list(reader)
-            for i, prop_value in enumerate(data_list):
-                for value in prop_value: 
-                    if value == id: 
-                        data_list[i][col] = newvalue 
+            for index, property_value in enumerate(data_list):
+                for value in property_value: 
+                    if value == prop_id: 
+                        data_list[index][col] = new_value 
         with open(self.filepath, "w", newline="", encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)  # converts the value into delimited string on the csvfile
             writer.writerows(data_list)
