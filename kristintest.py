6611c47b642c4c911ce98cf00e2fd8f690a288fa
kristinbodvars <kristinb20@ru.is>
2021-12-07 11:21:37 +0000

    kristintest

--- kristintest.py
+++ kristintest.py
@@ -19,7 +19,7 @@ from datetime import timedelta
 print(x.strftime("%A"))
 print(x.strftime("%D"))
 
-answer = input("Do you want to repeat this work request?")
+answer = int(input(("Do you want to repeat this work request?")))
 # Svarmöguleikar : 1. do not repeat
 #                  2. daily
 #                  3. weekly
@@ -27,50 +27,45 @@ answer = input("Do you want to repeat this work request?")
 #                  5. yearly
 date_ = input("When do yo want this to start? dd/mm/yyyy: ")
 date__ = date_.split('/')
-x = datetime(int(date__[2]),int(date__[1]),int(date__[0]))
+x = date(int(date__[2]),int(date__[1]),int(date__[0]))
+print(date.today())
 if date.today() < x:
-
+    
     if answer == 1:
         pass
     # create_work_request()
 
     elif answer == 2:
-        how_many = input('for how many days?:')
+        
+        how_many = int(input('for how many days?:'))
         for i in range(how_many):
             new_date = timedelta(days = 1*i)
             date_work_req = x + new_date
             print(date_work_req)
-            create_work_request()
+            # create_work_request()
 
     elif answer == 3:
-        how_many = input('for how many weeks?:')
+        how_many = int(input('for how many weeks?:'))
         for i in range(how_many):
-            data_list = []
-            y = datetime(int(date__[2]),int(date__[1]),int(date__[0]+7*i))
-
-
+            new_date = timedelta(weeks = 1*i)
+            date_work_req = x + new_date
 
     elif answer == 4:
         how_many = input('for how many months?:')
         for i in range(how_many):
-            data_list = []
-            y = datetime(int(date__[2]),int(date__[1]+1*i),int(date__[0]))
+            new_date = timedelta(months = 1*i)
+            date_work_req = x + new_date
 
     elif answer == 5:
         how_many = input('for how many years?:')
         for i in range(how_many):
-            data_list = []
-            y = datetime(int(date__[2]+1*i),int(date__[1]),int(date__[0]))
+            new_date = timedelta(years = 1*i)
+            date_work_req = x + new_date
+
 else:
     print("This date has already taken place")
 
 
-
-í_Dag = date.today()
-print(í_Dag)
-date__ = date_.split('/')
-# x = datetime.date(date_[2],date_[1],date_[0])
-print(x)
 
 
 
@@ -79,13 +74,13 @@ print(x)
 # date = 
 # Repeat = 
 # daglega
-d = d + 1
+# d = d + 1
 # vikulega
-d = d + 7
+# d = d + 7
 #mánaðarlega 
-m = m+1
+# m = m+1
 # árlega
-y = y+1
+# y = y+1
 
 
 
