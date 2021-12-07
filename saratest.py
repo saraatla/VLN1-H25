6095b5f11b3a66519a2f9c9c389a5797d94d524a
saraatla <saraatlala@gmail.com>
2021-12-07 14:30:51 +0000

    sara test

--- saratest.py
+++ saratest.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 from data.work_report_dl import WorkReportDL
 from models.work_report import WorkReport
 
@@ -33,8 +34,7 @@ for i in x:
     repDL.create_work_report(work_rep)"""
 
 
-from datetime import date
-from datetime import timedelta
+from datetime import date, timedelta
 
 from os import read
 from Extra.texttableFile.texttable import Texttable
@@ -50,50 +50,52 @@ class WorkRequestLL:
         self.table = Texttable()
         self.table2 = Texttable()
 
-    def create_work_request(self):
-            # return self.dlapi.create_work_request(work_req)
-            print('Enter the following information: ')
-            print(LINE)
-            last_id = self.dlapi.find_last_id()
-            new_id = int(last_id[1:])+1
-            workreq = [f'w{new_id}']
-            fieldnames = ["Title", "Property_ID"] #, "Destination", "Contractor", "Date", "Status", "Priority", "Description", "Workreport_ID"
-            for field in fieldnames:
-                val = input(f'{field}: ')
-                workreq.append(val)
-            workreq.append(self.destination)
-            contractor = input('Is a contractor needed for this work request: ')
-            workreq.append(contractor)
-            # gera date
-            start_date = input('Start date, dd/mm/yyyy: ')
-            workreq.append(start_date)
-            if start_date == date.today():
-                status = 'open'
-            else:
-                status = 'closed'
-            workreq.append(status)
-            priority_options  = ['Emergengy', 'Now', 'ASAP']
-            for i, option in enumerate(priority_options):
-                print(f"{i+1}: {option}")
-            index = int(input('Choose priority:'))
-            workreq.append(priority_options[index-1])
-            description = input('Description:  ')
-            workreq.append(description)
-            report_id = 'None'
-            workreq.append(report_id)
-            intro = print('Do you want to repeat this work request?')
-            options = ['Do no repeat', 'Daily', 'Weekly', 'Monthly', 'Yearly']
-            for i, option in enumerate(options):
-                print(f"{i+1}: {option}")
-            repeat = int(input('Choose option: '))
-            print(repeat)
-            if repeat > 1:
-                self.repeat_work_request(start_date, repeat, workreq)
-            else:
-                self.dlapi.create_work_request(WorkRequest(workreq))
-                print(f'{LINE}\nWork request successfully created!\n{LINE}')
 
-            
+    def create_work_request(self):
+        print('Enter the following information: ')
+        print(LINE)
+        start_date, workreq = self.create_work_req_list()
+        print('Do you want to repeat this work request?')
+        options = ['Do no repeat', 'Daily', 'Weekly', 'Monthly', 'Yearly']
+        for i, option in enumerate(options):
+            print(f"{i+1}: {option}")
+        repeat = int(input('Choose option: '))
+        if repeat > 1:
+            self.repeat_work_request(start_date, repeat, workreq)
+        else:
+            self.dlapi.create_work_request(WorkRequest(workreq))
+            print(f'{LINE}\nWork request successfully created!\n{LINE}')
+
+
+    def create_work_req_list(self):
+        last_id = self.dlapi.find_last_id()
+        new_id = int(last_id[1:])+1
+        workreq = [f'w{new_id}']
+        fieldnames = ["Title", "Property_ID"]
+        for field in fieldnames:
+            val = input(f'{field}: ')
+            workreq.append(val)
+        workreq.append(self.destination)
+        contractor = input('Is a contractor needed for this work request: ')
+        workreq.append(contractor)
+        start_date = input('Start date, dd/mm/yyyy: ')
+        workreq.append(start_date)
+        if start_date == date.today():
+            status = 'open'
+        else:
+            status = 'closed'
+        workreq.append(status)
+        priority_options  = ['Emergengy', 'Now', 'ASAP']
+        for i, option in enumerate(priority_options):
+            print(f"{i+1}: {option}")
+        index = int(input('Choose priority:'))
+        priority = priority_options[index-1]
+        workreq.append(priority)
+        description = input('Description:  ')
+        workreq.append(description)
+        report_id = 'None'
+        workreq.append(report_id)
+        return start_date, workreq
 
 
     def repeat_work_request(self,start_date, repeat, workreq):
@@ -102,69 +104,69 @@ class WorkRequestLL:
         if date.today() <= x:
             if repeat == 2:
                 how_many = int(input('for how many days?:'))
-                for i in range(how_many):
-                    new_date = timedelta(days = 1*i)
-                    date_work_req = x + new_date
-                    date_work_req = date_work_req.strftime('%d/%m/%Y')
-                    workreq[5] = date_work_req
-                    self.dlapi.create_work_request(WorkRequest(workreq))
+                self.daily_repeat(how_many, workreq, x)
             elif repeat == 3:
                 how_many = int(input('for how many weeks?:'))
-                for i in range(how_many):
-                    new_date = timedelta(weeks = 1*i)
-                    date_work_req = x + new_date
-                    date_work_req = date_work_req.strftime("%D/%M/%Y")
-                    workreq[5] = date_work_req
-                    self.dlapi.create_work_request(WorkRequest(workreq))
+                self.weekly_repeat(how_many, workreq, x)
             elif repeat == 4:
-                how_many = input('for how many months?:')
-                for i in range(how_many):
-                    new_date = timedelta(months = 1*i)
-                    date_work_req = x + new_date
-                    date_work_req.strftime("%D/%M/%Y")
-                    workreq[5] = date_work_req
-                    self.dlapi.create_work_request(WorkRequest(workreq))
+                how_many = int(input('for how many months?:'))
+                self.monthly_repeat(how_many, workreq, x)
             elif repeat == 5:
-                how_many = input('for how many years?:')
-                for i in range(how_many):
-                    new_date = timedelta(years = 1*i)
-                    date_work_req = x + new_date   
-                    date_work_req.strftime("%D/%M/%Y")
-                    workreq[5] = date_work_req
-                    self.dlapi.create_work_request(WorkRequest(workreq))  
+                how_many = int(input('for how many years?:'))
+                self.yearly_repeat(how_many, workreq, x)
         else:
             print("Can not create work request in the past")
 
+
+    def daily_repeat(self, how_many, workreq, x):
+        for i in range(how_many):
+            new_date = timedelta(days = 1*i)
+            date_work_req = x + new_date
+            date_work_req = date_work_req.strftime('%d/%m/%Y')
+            workreq[5] = date_work_req
+            self.dlapi.create_work_request(WorkRequest(workreq))
+            old_id = workreq[0]
+            new_id = int(old_id[1:])+1
+            workreq[0] = f'w{new_id}'
+
+
+    def weekly_repeat(self, how_many, workreq, x):
+        for i in range(how_many):
+            new_date = timedelta(weeks = 1*i)
+            date_work_req = x + new_date
+            date_work_req = date_work_req.strftime('%d/%m/%Y')
+            workreq[5] = date_work_req
+            self.dlapi.create_work_request(WorkRequest(workreq))
+            old_id = workreq[0]
+            new_id = int(old_id[1:])+1
+            workreq[0] = f'w{new_id}'
+    
+
+    def monthly_repeat(self, how_many, workreq, x):
+        for i in range(how_many):
+            new_date = timedelta(months = 1*i)
+            date_work_req = x + new_date
+            date_work_req = date_work_req.strftime('%d/%m/%Y')
+            workreq[5] = date_work_req
+            self.dlapi.create_work_request(WorkRequest(workreq))
+            old_id = workreq[0]
+            new_id = int(old_id[1:])+1
+            workreq[0] = f'w{new_id}'
+
+
+    def yearly_repeat(self, how_many, workreq, x):
+        for i in range(how_many):
+            new_date = timedelta(years = 1*i)
+            date_work_req = x + new_date   
+            date_work_req = date_work_req.strftime('%d/%m/%Y')
+            workreq[5] = date_work_req
+            self.dlapi.create_work_request(WorkRequest(workreq)) 
+            old_id = workreq[0]
+            new_id = int(old_id[1:])+1
+            workreq[0] = f'w{new_id}' 
+
         
-    """def repeat_work_request(self,start_date, repeat, workreq):
-        dates = start_date.split('/')
-        x = date(int(dates[2]),int(dates[1]),int(dates[0]))
-        if date.today() <= x:
-            if repeat == 2:
-                how_many = int(input('for how many days?:'))
-                time_val = 'days'
-                
-            elif repeat == 3:
-                how_many = int(input('for how many weeks?:'))
-                time_val = 'weeks'
-                
-            elif repeat == 4:
-                how_many = input('for how many months?:')
-                time_val = 'months'
-                
-            elif repeat == 5:
-                how_many = input('for how many years?:')
-                time_val = 'years'
-                
-            for i in range(how_many):
-                    new_date = timedelta(time_val = 1*i)
-                    date_work_req = x + new_date
-                    date_work_req.strftime("%D/%M/%Y")
-                    workreq[5] = date_work_req
-                    self.dlapi.create_work_request(WorkRequest(workreq))
-            
-        else:
-            print("Can not create work request in the past")"""
+
 
 
 wLL  = WorkRequestLL('Svalbard')
