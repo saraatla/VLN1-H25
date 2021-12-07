feb6cdbf99d7b89ef9ea3d530a6731572c2b472e
kristinbodvars <kristinb20@ru.is>
2021-12-07 14:16:32 +0000

    contractor

--- ui/contractor_ui.py
+++ ui/contractor_ui.py
@@ -7,32 +7,44 @@ class ContractorUI:
         self.llapi= LLAPI(self.destination)
         self.contractor = contractor
         self.user_type = user_type
-        if self.user_type == 'Manager':
-            self.options = """1: Edit
-B: Back"""
-            self.input_str = "Choose Options edit or back: "
-        else:
-            self.options = """B: Back"""
-            self.input_str = "Press B for back: "
 
     def start(self):
         print(LINE)
         print(self.contractor)
         print(LINE)
         while True:
-            print(self.options)
-            print(LINE)
+            if self.user_type == 'Manager':
+                self.manager_start()
+            elif self.user_type == 'Employee':
+                self.employee_start()
+                print(LINE)
 
-            commands2 = input(self.input_str).upper()
-            print(LINE)
-            if commands2 == "1":
+    def manager_start(self):
+        options = """1: Edit \nB: Back"""
+        input_str = "Choose Options edit or back: "
+        print(options)
+        print(LINE)
+        command = input(input_str).upper()
+        print(LINE)
+        if command == "1":
                 self.llapi.edit_contractor(self.contractor)
                 edited_contractor = self.llapi.search_contractor(self.contractor.id)
                 print(LINE)
                 print(edited_contractor)
                 print(LINE)
-            elif commands2 == "B":
-                return
-            else:
-                print("Invalid option, try again ")
-                print(LINE)
+        elif command == "B":
+            return
+        else:
+            print("Invalid option, try again ")
+        
+    def employee_start(self):
+        options = """B: Back"""
+        input_str = "Press B for back: "
+        print(options)
+        print(LINE)
+        command = input(input_str).upper()
+        print(LINE)
+        if command == "B":
+            return
+        else:
+            print("Invalid option, try again ")
