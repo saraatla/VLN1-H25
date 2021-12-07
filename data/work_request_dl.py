9ade860617c9026da036c695a942535a627f9dc3
kristinbodvars <kristinb20@ru.is>
2021-12-07 11:56:47 +0000

    work request bætti við workreport id

--- data/work_request_dl.py
+++ data/work_request_dl.py
@@ -19,18 +19,18 @@ class WorkRequestDL:
                                              # are given by the optional fieldnames parameter.
             for row in reader:
                 work_req = WorkRequest([row["Workrequest_ID"], row["Title"], row["Property_ID"], row["Destination_ID"],
-                row["Contractor"], time(row["Date"].split('/')[2],row["Date"].split('/')[1],row["Date"].split('/')[0]), row["Status"], row["Priority"], row["Description"], row["Workreport_ID"]])
+                row["Contractor"], (time(row["Date"].split('/')[2],row["Date"].split('/')[1],row["Date"].split('/')[0])), row["Status"], row["Priority"], row["Description"], row["Workreport_ID"]])
                 ret_list.append(work_req)
         return ret_list
 
     def create_work_request(self,work_req):
         "This function appends a new work request to the csv file"
         with open(self.filepath, 'a', newline='') as csvfile:
-            fieldnames = ['Workrequest_ID', 'Title', 'Property_ID', 'Destination_ID', 'Contractor', 'Date', 'Status','Priority','Description']
+            fieldnames = ['Workrequest_ID', 'Title', 'Property_ID', 'Destination', 'Contractor', 'Date', 'Status','Priority','Description','Workreport_ID']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames) # writer maps dictionaries onto output rows.
             writer.writerow({'Workrequest_ID': work_req.workrequest_id, 'Title': work_req.title, 'Property_ID': work_req.property_id, 
-            'Destination_ID': work_req.destination_id, 'Contractor':work_req.contractor, 'Date':work_req.date, 
-            'Status':work_req.status, 'Priority':work_req.priority, 'Description':work_req.description})
+            'Destination': work_req.destination, 'Contractor':work_req.contractor, 'Date':work_req.date, 
+            'Status':work_req.status, 'Priority':work_req.priority, 'Description':work_req.description, 'Workreport_ID':work_req.workreport_id})
     
     def edit_work_request(self, work_reqno, col,newvalue ):  
         """This function edits a certain value for a certain work request (input by supervisor)"""
