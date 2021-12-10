ab7442ba8e4136c549f69c7e583acde051cba19d
saraatla <saraatlala@gmail.com>
2021-12-10 13:48:00 +0000

    date villu melding work request

--- ui/work_requests_ui.py
+++ ui/work_requests_ui.py
@@ -363,10 +363,11 @@ class WorkRequestUI:
         workreq.append(contractor)
         while True:
             start_date = self.__check_date('start date')
-            if date.today() <= start_date:
-                break
-            else:
+            date_var = datetime.strptime(start_date,'%d/%m/%Y').date()
+            if date.today() > date_var:
                 print('Can not create work request in the past')
+            else:
+                break
         workreq.append(start_date)
         if start_date == date.today():
             status = 'open'
@@ -462,5 +463,7 @@ class WorkRequestUI:
                 return date_string
             except ValueError:
                 print("This is the incorrect date format. It should be dd/mm/yyyy")
+        
+            
 
 
